8b17b99752a3ab45c2cdb2d115a381dc16f3596c
theweakestskid <122355099+theweakestskid@users.noreply.github.com>
2024-07-20 20:04:37 +0700

    Create funnycommandstuff.lua

---
+++ funnycommandstuff.lua
@@ -0,0 +1,12 @@
+if not _G.Commands then return end
+local plrs=game.Players
+local lp=plrs.LocalPlayer
+
+lp.Chatted:Connect(function(c)
+    local command, arg = c:match("^(%S+)%s*(.*)$")
+    if _G.Commands:command then
+        _G.Commands[command](arg)
+    end
+end)
+
+--super duper short script by lazed_1
