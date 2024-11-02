8c62b7a931208ed4f360fbc1fc1b98948f7bd782
theweakestskid <122355099+theweakestskid@users.noreply.github.com>
2024-11-02 08:15:39 +0700

    Update V1encoderSimplified.lua

--- coding stuffz/V1encoderSimplified.lua
+++ coding stuffz/V1encoderSimplified.lua
@@ -19,12 +19,12 @@ function l.random_numbers_and_letter(length) ret = "" for i = 0, length do ret =
 function l.encode(data) repeat wait() until ts return ts:JSONEncode(data) end;
 function l.decode(data) repeat wait() until ts return ts:JSONDecode(data) end;
 function l.wf_enc(n, dat) wf(n, encode(dat)) if isfile(n) then print("made and encoded " .. n) else warn("cannot make file") end end;
-function l.dec_file(f) if isfile(f) then return decode(readfile(f)) end end;
+function l.dec_file(f) if isfile(f) then return l.decode(readfile(f)) end end;
 function l.decsave_file(f) if isfile(f) then wf(f, dec_file(f)) end end;
-function l.save_table(n, tbl) wf_enc(n, tbl) if isfile(n) then decsave_file(n) end end;
+function l.save_table(n, tbl) wf_enc(n, tbl) if isfile(n) then l.decsave_file(n) end end;
 function l.concat_table(tbl) return "{'" .. n .. "', \"" .. table.concat(tbl, '", "') .. "\"}" end;
 function l.call_in_file(n) if isfile(n) then return dofile(n) end end;
-function l.rna(nm) if nm then random_numbers_and_letter(nm) else return end end end
+function l.rna(nm) if nm then return l.random_numbers_and_letter(nm) else return end end end
 return l
 --/End Main Functions\--
 --{         Nothing much of An Script          }--
